62818bc8c5c9554de21e8a1a813be0b4dddd655c
Wajdan126 <rasoolwajdan@gmail.com>
2026-02-17 01:39:00 +0500

    Wajdan's Commit

---
+++ app/src/main/java/com/example/nullex/Square.java
@@ -0,0 +1,18 @@
+package com.example.nullex;
+
+public class Square extends Shape {
+
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+}
